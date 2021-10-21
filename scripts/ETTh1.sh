bb47dd1063ac41dc4ec3a3bbd6bb950d446aabd3
Jimmie Goode <jimmiegoode@gmail.com>
2021-10-21 21:21:53 +0000

    Run ETTh2 again

--- scripts/ETTh1.sh
+++ scripts/ETTh1.sh
@@ -9,7 +9,7 @@ ITERS=1
 
 ### M
 
-python -u main_informer.py --model informer --data ETTh1 --features M --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn full --des $DES --itr $ITERS --factor 3 --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
+python -u main_informer.py --model informer --data ETTh1 --features M --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn full --factor 3 --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
 python -u main_informer.py --model informer --data ETTh1 --features M --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn full --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
--- scripts/ETTh2.sh
+++ scripts/ETTh2.sh
@@ -1,23 +1,32 @@
+set -e
+
+GPU=$1
+ENC_TYPE=$2
+DEC_TYPE=$3
+DES=$4
+
+ITERS=1
+
 ### M
 
-python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 96 --label_len 96 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 96 --label_len 96 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 336 --label_len 336 --pred_len 168 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 336 --label_len 336 --pred_len 168 --e_layers 3 --d_layers 2 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 336 --label_len 168 --pred_len 336 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 336 --label_len 168 --pred_len 336 --e_layers 3 --d_layers 2 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 720 --label_len 336 --pred_len 720 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features M --seq_len 720 --label_len 336 --pred_len 720 --e_layers 3 --d_layers 2 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
 ### S
 
-python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 5 --factor 3
+python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 48 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --factor 3 --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 96 --label_len 96 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 96 --label_len 96 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 336 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 336 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 168 --pred_len 336 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 168 --pred_len 336 --e_layers 3 --d_layers 2 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
 
-python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 336 --pred_len 720 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --itr 5
+python -u main_informer.py --model informer --data ETTh2 --features S --seq_len 336 --label_len 336 --pred_len 720 --e_layers 3 --d_layers 2 --attn prob --des $DES --itr $ITERS --gpu $GPU --encoder_type $ENC_TYPE --decoder_type $DEC_TYPE
